0a70b2fc943c89a564a3d37a6b6c5a18d1aa5341
Nigel van Keulen <nigel2392@gmail.com>
2024-03-18 17:30:27 +0100

    Utility functions

--- conditional_field/__init__.py
+++ conditional_field/__init__.py
@@ -1,8 +1,19 @@
 class _Value(str):
     pass
 
+class _Action(str):
+    pass
+
 ANY = _Value("any")
 EMPTY = _Value("empty")
+
+SHOW = _Action("show")
+HIDE = _Action("hide")
+FSHOW = _Action("fshow")
+FHIDE = _Action("fhide")
+
+def classname(*args: str) -> str:
+    return " ".join(args)
 
 def handler(name: str) -> str:
     return f"gcf gcf-handler--{name}"
